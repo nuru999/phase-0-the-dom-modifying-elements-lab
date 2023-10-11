ee8bbd3858878d651452e3c1cd203f789bb33862
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-10-11 11:22:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+//Start the code here //
+let mainElement = document.querySelector('main#main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Amudi is the champion";
+
+document.body.appendChild(newHeader);
